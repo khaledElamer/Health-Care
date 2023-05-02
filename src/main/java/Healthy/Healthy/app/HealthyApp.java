8762b7f0fa37cb9cb1762b9c1a86394c43e3d465
khaled mohamed <khaled.mohamed@segmatek.com>
2023-05-03 02:06:57 +0300

    add required entities and implement it

--- src/main/java/Healthy/Healthy/app/HealthyApp.java
+++ src/main/java/Healthy/Healthy/app/HealthyApp.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class HealthyAppApplication {
+public class HealthyApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthyAppApplication.class, args);
+		SpringApplication.run(HealthyApp.class, args);
 	}
 
 }
